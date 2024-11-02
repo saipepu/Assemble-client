4723f64252e7ba9efcea66288b46781bb488e446
saipepu <saipepu.mdy257@gmail.com>
2024-11-02 09:31:20 +0700

    Refactor

--- auth.ts
+++ auth.ts
@@ -1,86 +1,79 @@
-import NextAuth from 'next-auth'
-import Google from 'next-auth/providers/google'
-import Github from 'next-auth/providers/github'
-import { api } from '@/app/api/index'
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import { api } from "@/app/api/index";
 
-declare module 'next-auth' {
-
+declare module "next-auth" {
   interface Session {
     accessToken: string;
+    assembleUser: any;
   }
   interface User {
     accessToken?: string;
+    assembleUser: any;
   }
 }
-
-// declare module 'next-auth/jwt' {
-//   interface JWT {
-//     accessToken?: string;
-//   }
-// }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
       authorization: {
         params: {
-          prompt: "select_account"
-        }
-      }
+          prompt: "select_account",
+        },
+      },
     }),
     Github({
       authorization: {
         params: {
-          prompt: "select_account"
-        }
-      }
-    })
+          prompt: "select_account",
+        },
+      },
+    }),
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
       try {
-
         let dto = {
-          "firstName": user.name,
-          "email": user.email,
-          "profile": user.image,
-          "password": user.email
-        }
+          firstName: user.name,
+          email: user.email,
+          profile: user.image,
+          password: user.email,
+        };
 
         const response = await fetch(`${api}/sign-up`, {
           method: "POST",
           headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
+            Accept: "application/json",
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(dto)
+          body: JSON.stringify(dto),
         })
-        .then(response => response.json())
-        .catch(error => console.log(error))
+          .then((response) => response.json())
+          .catch((error) => console.log(error));
 
-        
-        if(response?.success) {
-
-          user.accessToken = response.message.accessToken
-          return true
-          
+        if (response?.success) {
+          user.accessToken = response.message.accessToken;
+          user.assembleUser = response.message.user;
+          return true;
         }
-        return false
-
-      } catch(error) {
-        console.log(error, 'error')
-        return false
+        return false;
+      } catch (error) {
+        console.log(error, "error");
+        return false;
       }
     },
-    async jwt({token, user, account, profile}) {
+    async jwt({ token, user, account, profile }) {
       if (user) {
-        token.accessToken = user.accessToken
+        token.accessToken = user.accessToken;
+        token.assembleUser = user.assembleUser;
       }
-      return token
+      return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken as string
-      return session
-    }
-  }
-})
+      session.accessToken = token.accessToken as string;
+      session.assembleUser = token.assembleUser;
+      return session;
+    },
+  },
+});
